a4f8751beb9bb52e7eea920a4be7b24bc43b82ab
Enio <enio7.ribeiro@gmail.com>
2021-04-23 09:57:08 -0300

    changing method name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
 
     public function getUsers(){
 
-        return $this->UserService->getAllUsers();
+        return $this->UserService->getAll();
 
     }
 
