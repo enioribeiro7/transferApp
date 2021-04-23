c5a3387902d0e6be7d1bdfbf1720849385c5cb8f
Enio <enio7.ribeiro@gmail.com>
2021-04-23 00:33:00 -0300

    testing NotificationServiceTest class

---
+++ tests/Unit/Services/NotificationServiceTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Clients\NotificationClient;
+use App\Jobs\NotificationTransferEmailJob;
+use App\Notification\NotificationResult;
+use App\Services\NotificationService;
+use App\User;
+use PHPUnit\Framework\TestCase;
+
+class NotificationServiceTest extends TestCase
+{
+
+    public function testSentShouldReturnTrueWhenNotificationWasSent()
+    {
+        $amount = 100;
+        $from = new User();
+        $to = new User();
+
+        $notificationResult = $this->getMockBuilder(NotificationResult::class)
+            ->setMethods(['wasSent'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $notificationResult->expects($this->once())
+            ->method('wasSent')
+            ->willReturn(true);
+
+        $notificationClient = $this->getMockBuilder(NotificationClient::class)
+            ->setMethods(['sentNotification'])
+            ->getMock();
+        $notificationClient->expects($this->once())
+            ->method('sentNotification')
+            ->with($from, $to, $amount)
+            ->willReturn($notificationResult);
+
+        $notificationTransferEmailJob = $this->getMockBuilder(NotificationTransferEmailJob::class)
+            ->setMethods(['trigger'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $notificationTransferEmailJob->expects($this->never())
+            ->method('trigger')
+            ->with($from, $to, $amount);
+            
+        $notificationService = new NotificationService($notificationClient, $notificationTransferEmailJob);
+        $result = $notificationService->sent($from, $to, $amount);
+
+        $this->assertEquals(true, $result);
+        
+        
+    }
+
+    public function testSentShouldReturnFalseWhenNotificationWasNotSent()
+    {
+        $amount = 100;
+        $from = new User();
+        $to = new User();
+
+        $notificationResult = $this->getMockBuilder(NotificationResult::class)
+            ->setMethods(['wasSent'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $notificationResult->expects($this->once())
+            ->method('wasSent')
+            ->willReturn(false);
+
+        $notificationClient = $this->getMockBuilder(NotificationClient::class)
+            ->setMethods(['sentNotification'])
+            ->getMock();
+        $notificationClient->expects($this->once())
+            ->method('sentNotification')
+            ->with($from, $to, $amount)
+            ->willReturn($notificationResult);
+
+        $notificationTransferEmailJob = $this->getMockBuilder(NotificationTransferEmailJob::class)
+            ->setMethods(['trigger'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $notificationTransferEmailJob->expects($this->once())
+            ->method('trigger')
+            ->with($from, $to, $amount);
+            
+        $notificationService = new NotificationService($notificationClient, $notificationTransferEmailJob);
+        $result = $notificationService->sent($from, $to, $amount);
+
+        $this->assertEquals(false, $result);
+        
+        
+    }
+  
+}
