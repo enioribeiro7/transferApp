40d88f0b4966703a4d93657f20b6374204081d11
Enio <enio7.ribeiro@gmail.com>
2021-04-23 10:08:26 -0300

    refactoring class

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -5,26 +5,29 @@ namespace App\Services;
 use App\Clients\NotificationClient;
 use App\Jobs\NotificationTransferEmailJob;
 use Illuminate\Support\Facades\Mail;
+use App\User;
 
 class NotificationService 
 {
 
-    public function __construct(NotificationClient $notificationClient) 
-    {
+    public function __construct(
+        NotificationClient $notificationClient,
+        NotificationTransferEmailJob $notificationTransferEmailJob
+    ) {
         $this->notificationClient = $notificationClient;
+        $this->notificationTransferEmailJob = $notificationTransferEmailJob;
     }
 
 
-    public function sent($from, $to, $amount)
+    public function sent(User $from,User $to, float $amount)
     {
         $result = $this->notificationClient->sentNotification($from, $to, $amount);
+        $wasSent = $result->wasSent();
 
-        if ($result->wasSent() != true) {
-
-            //Agenda notificação por email
-            NotificationTransferEmailJob::dispatch($from, $to, $amount)->delay( now()->addSeconds('15'));
+        if (!$wasSent) {
+            $this->notificationTransferEmailJob->trigger($from, $to, $amount);
         }
 
-        return $result->wasSent();
+        return $wasSent;
     }
 }    
